2c621d5da7bd836569d0f74005ac8721d3570674
Antti Ahti <antti.ahti@gmail.com>
2018-05-20 12:23:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Minimal project for trying to reproduce issue https://github.com/corygibbons/component-image/issues/9
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => (
+  <div>
+    <h1>Hello world / default font</h1>
+    <h1 className="font-kavivanar">Hello world / Kavivanar</h1>
+    <h1 className="font-modern-antiqua">Hello world / Modern Antiqua</h1>
+  </div>
+);
+
+export default App;
